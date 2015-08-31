ed410903a0373d7c0ec0b6e9d64e50f9ee5ae162
Mike Conigliaro <mike@conigliaro.org>
2015-08-31 15:44:23 -0600

    Add introduction

---
+++ manuscript/introduction.rb
@@ -0,0 +1,10 @@
+# Introduction
+
+## How to use this book
+
+## Command line conventions
+
+- Angle brackets (`<>`) surround required arguments
+- Square rackets (`[]`) surround optional arguments
+- Vertical bars (`|`) separate choices
+- Ellipses (`...`) can be repeated
